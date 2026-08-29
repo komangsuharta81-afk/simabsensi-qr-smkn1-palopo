21d27f928026800654968000c3aec3f0f5a1d5d0
komangsuharta81-afk <komangsuharta81@guru.smk.belajar.id>
2026-08-29 21:06:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // Ganti dengan URL Web App Google Apps Script yang berakhiran /exec
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbwekLgPk0uh2hl38N6EUbBWeWk8pLQdGTRTzZkcLBFZEW8lHGYK3az-SuFbt4rBgLc1/exec
-';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxB2xlDFPVllrktYAQUybgPAa5zIub2TKi-OVNBvSC14UwQw_oSjSj3H8Kr2peW0GkV/exec';
 const APP_CONFIG = {
   name:'SIABSEN QR',
   school:'SMK NEGERI 1 PALOPO',
